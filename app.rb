65c6008a2c9856c68c17e57456764d44d313d23c
Martin Lupa <martinlupa@Martins-MacBook-Pro.local>
2025-08-08 11:42:16 +1000

    feat: cleanup

--- app.rb
+++ app.rb
@@ -24,24 +24,6 @@ Cuba.define do
     end
   end
 
-  on get, 'authors' do
-    authors = File.read('AUTHORS')
-
-    res.headers['cache-control'] = 'public, max-age=86400' # 24 hours
-    res.headers['expires'] = (Time.now + 24).to_s
-
-    res.json({ authors: authors })
-  end
-
-  on get, 'openapi.yaml' do
-    openapi_spec = File.read('openapi.yaml')
-
-    res.headers['cache-control'] = 'no-store, no-cache, must-revalidate'
-    res.headers['expires'] = Time.now.to_s
-
-    res.json({ openapi_spec: openapi_spec })
-  end
-
   on 'products' do
     valid_session = auth_service.validate_session(req.get_header('HTTP_AUTHORIZATION'))
 
@@ -61,7 +43,7 @@ Cuba.define do
       end
     else
       res.status = 401
-      res.json({ message: '[Authorization header] invalid or expired token' })
+      res.json({ error: 'invalid or expired token' })
     end
   end
 end
--- config.ru
+++ config.ru
@@ -9,6 +9,15 @@ require './app'
 
 app = Rack::Builder.new do
   use Rack::Deflater # Enables the Accept-Encoding header for gzip
+  use Rack::Static, {
+    root: '.',
+    urls: ['/AUTHORS', '/openapi.yaml'],
+    headers_rules: [
+      ['/AUTHORS', { 'cache-control' => 'public, max-age=86400', 'expires' => Time.now + 24 }],
+      ['/openapi.yaml', { 'cache-control' => 'no-cache, must-revalidate', 'expires' => Time.now }]
+    ]
+  }
+  use Rack::Sendfile # https://www.rubydoc.info/gems/rack/Rack/Sendfile for nginx config
   run Cuba
 end
 
